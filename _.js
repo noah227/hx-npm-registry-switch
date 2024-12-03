f64566afaaf1d21fe7245b5d3ed797cf40487b16
noah227 <noah227@foxmail.com>
2024-12-03 14:29:13 +0800

    style: 格式化代码（缩进相关）

--- _.js
+++ _.js
@@ -1,99 +1,105 @@
 const {
-	getRegistries,
-	getCurrentRegistry,
-	readFile,
-	writeFile
+    getRegistries,
+    getCurrentRegistry,
+    readFile,
+    writeFile
 } = require("nrm/helpers")
-const {NPMRC} = require("nrm/constants")
+const {
+    NPMRC
+} = require("nrm/constants")
 const hx = require("hbuilderx")
 const fs = require("fs")
-const {execSync} = require("child_process")
+const {
+    execSync
+} = require("child_process")
 
 async function getRegistryName() {
     // 这是一个网络地址（实际的源）
-	let registry = await getCurrentRegistry()
+    let registry = await getCurrentRegistry()
     // 如果.npmrc不存在，那么nrm获取registry返回为空
     // 此时，尝试以传统模式获取npm配置的源
-    if(!registry) {
+    if (!registry) {
         console.log("没有获取到registry，尝试以传统配置模式获取")
         registry = getRegistryNameClassic()
-        if(registry) console.log(`传统配置模式获取成功：${registry}`)
+        if (registry) console.log(`传统配置模式获取成功：${registry}`)
     }
-	const registries = await getRegistries()
-	const registryMatch = Object.entries(registries).find(([k, data]) => {
-		return data.registry === registry
-	})
-	return registryMatch ? registryMatch[0] : null
+    const registries = await getRegistries()
+    const registryMatch = Object.entries(registries).find(([k, data]) => {
+        return data.registry === registry
+    })
+    return registryMatch ? registryMatch[0] : null
 }
 
 // 传统方式获取注册源
-function getRegistryNameClassic(){
+function getRegistryNameClassic() {
     const registry = execSync("npm config --global get registry").toString("utf8")
     return registry.replace(/\n$/, "")
 }
 
 const SHOW_COMMAND = "ny.show-nrm"
 module.exports = {
-	barItem: null,
-	async initBarItem(text, priority = 0) {
-		text = text || await getRegistryName()
-		const barItem = hx.window.createStatusBarItem(
-			hx.StatusBarAlignment.Right, priority
-		)
-		barItem.text = "$(npm):" + text
-		barItem.command = SHOW_COMMAND
-		barItem.hide()
-		barItem.show()
-		console.log(Object.keys(barItem))
-		this.barItem = barItem
-		return barItem
-	},
-	async updateBarItem(registry) {
-		this.barItem.text = "$(npm):" + (registry || await getRegistryName())
-	},
-	async updateRegistries(registries, registry){
-		const npmrc = await readFile(NPMRC)
-		await writeFile(NPMRC, Object.assign(npmrc, registries[registry]))
-	},
-	async registerCommand(context) {
-		context.subscriptions.push(
-			hx.commands.registerCommand(SHOW_COMMAND, async () => {
-				if (!this.barItem) await this.initBarItem()
-				console.log("EXECUTE")
-				const registries = await getRegistries()
-				// 怎么调用内置的contextmenu创建，我母鸡啊
-				hx.window.showQuickPick(Object.keys(registries).map(k => ({
-					label: k,
-					description: registries[k].registry
-				}))).then(data => {
-					const {label: registry} = data
-					console.log("选择了：", registry)
-					this.updateRegistries(registries, registry)
-					this.updateBarItem(registry)
-				})
-			})
-		)
-	},
-	/**
-	 * 监听外部变动同步（仅nrm全局模式）
-	 */
-	startWatch(){
-		fs.watchFile(NPMRC, async (curr, prev) => {
-			// 实际从npm配置取的
-			const registry = await getRegistryName()
-			if(registry !== this.barItem.text) this.updateBarItem()
-		})
-	},
-	/**
-	 * 随着主进程销毁而销毁？
-	 */
-	releaseWatch(){
-		
-	},
-	async init(context) {
-		console.log(await getRegistryName())
-		await this.initBarItem()
-		await this.registerCommand(context)
-		this.startWatch()
-	}
+    barItem: null,
+    async initBarItem(text, priority = 0) {
+        text = text || await getRegistryName()
+        const barItem = hx.window.createStatusBarItem(
+            hx.StatusBarAlignment.Right, priority
+        )
+        barItem.text = "$(npm):" + text
+        barItem.command = SHOW_COMMAND
+        barItem.hide()
+        barItem.show()
+        console.log(Object.keys(barItem))
+        this.barItem = barItem
+        return barItem
+    },
+    async updateBarItem(registry) {
+        this.barItem.text = "$(npm):" + (registry || await getRegistryName())
+    },
+    async updateRegistries(registries, registry) {
+        const npmrc = await readFile(NPMRC)
+        await writeFile(NPMRC, Object.assign(npmrc, registries[registry]))
+    },
+    async registerCommand(context) {
+        context.subscriptions.push(
+            hx.commands.registerCommand(SHOW_COMMAND, async () => {
+                if (!this.barItem) await this.initBarItem()
+                console.log("EXECUTE")
+                const registries = await getRegistries()
+                // 怎么调用内置的contextmenu创建，我母鸡啊
+                hx.window.showQuickPick(Object.keys(registries).map(k => ({
+                    label: k,
+                    description: registries[k].registry
+                }))).then(data => {
+                    const {
+                        label: registry
+                    } = data
+                    console.log("选择了：", registry)
+                    this.updateRegistries(registries, registry)
+                    this.updateBarItem(registry)
+                })
+            })
+        )
+    },
+    /**
+     * 监听外部变动同步（仅nrm全局模式）
+     */
+    startWatch() {
+        fs.watchFile(NPMRC, async (curr, prev) => {
+            // 实际从npm配置取的
+            const registry = await getRegistryName()
+            if (registry !== this.barItem.text) this.updateBarItem()
+        })
+    },
+    /**
+     * 随着主进程销毁而销毁？
+     */
+    releaseWatch() {
+
+    },
+    async init(context) {
+        console.log(await getRegistryName())
+        await this.initBarItem()
+        await this.registerCommand(context)
+        this.startWatch()
+    }
 }
