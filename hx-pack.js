7ce7073a81eef776edbf9ff9f3ed5654ff132d4d
NoahYoung <noah227@foxmail.com>
2025-06-16 00:00:44 +0800

    chore: 调整打包配置（字体只打包ttf，添加缺失的打包内容）

--- hx-pack.js
+++ hx-pack.js
@@ -2,8 +2,9 @@ const cozip = require("cozip")
 
 const pack = () => {
     cozip(`${require("./package.json").id}.zip`, [
-		["./fonts", true],
+		["./fonts/iconfont.ttf"],
         ["./_.js"],
+        ["./_nrm.js"],
         ["./node_modules", true],
         ["./extension.js"], 
         ["./package.json"]
